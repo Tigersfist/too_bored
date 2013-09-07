4c690d81de9278e1734ce077b34b260001e4f04f
Jordan Martin <jordanmartin.asme@seas.upenn.edu>
2013-09-07 13:59:12 -0400

    Created rails application and added status update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TooBored::Application.config.secret_token = 'e336dce655f5a950601447e68fa36ff6f4fdad97fcac87d59380c80d4dd0818f19f0148be1869116eba38f0502c42b994844a15470b5d6d702eeb0b7c8fb3983'
